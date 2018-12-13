fc9b3826ce3f86f4b82f225a10426981ac9e2555
sjafri5 <raza.jafri.sy@gmail.com>
2018-12-13 10:22:07 -0600

    gitignore server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-//this is server.
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true  }));
+var path = require('path');
+//app.use(express.static(path.join(__dirname, './static')));
+//app.set('views', path.join(__dirname, './views'));
+app.use(express.static( __dirname + '/public/dist/public'  ));
+//app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+ res.render('index');
+})
+
+app.listen(8000, function() {
+console.log("listening on port 8000");
+})
